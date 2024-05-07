14a3bed46b3669da54417a615e346679c61a93cf
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2024-05-07 21:11:03 +0200

    feat: changed vars to properties

--- Assets/Networking/BaseResponseDTO.cs
+++ Assets/Networking/BaseResponseDTO.cs
@@ -1,43 +1,43 @@
 namespace Networking.DTO{
     public class BaseResponseDTO{
-        public string id;
-        public string _object;
-        public int created;
-        public string model;
-        public PromptFilterResult[] prompt_filter_results;
-        public Choice[] choices;
-        public Usage usage;
+        public string id {get;set;}
+        public string _object {get;set;}
+        public int created {get;set;}
+        public string model {get;set;}
+        public PromptFilterResult[] prompt_filter_results {get;set;}
+        public Choice[] choices {get;set;}
+        public Usage usage {get;set;}
         
     }
 
     public class PromptFilterResult{
-        public int prompt_index;
-        public ContentFilterResults content_filter_results;
+        public int prompt_index {get;set;}
+        public ContentFilterResults content_filter_results {get;set;}
     }
 
     public class ContentFilterResult{
-        public bool filtered;
-        public string severity;
+        public bool filtered {get;set;}
+        public string severity {get;set;}
     }
 
     public class ContentFilterResults{
-        public ContentFilterResult hate;
-        public ContentFilterResult self_harm;
-        public ContentFilterResult sexual;
-        public ContentFilterResult violence;
+        public ContentFilterResult hate {get;set;}
+        public ContentFilterResult self_harm {get;set;}
+        public ContentFilterResult sexual {get;set;}
+        public ContentFilterResult violence {get;set;}
     }
 
     public class Choice{
-        public string finish_reason;
-        public int index;
-        public GPTRoles message;
-        public ContentFilterResults content_filter_results;
+        public string finish_reason {get;set;}
+        public int index {get;set;}
+        public GPTRoles message {get;set;}
+        public ContentFilterResults content_filter_results {get;set;}
     }
 
     public class Usage{
-        public int prompt_tokens;
-        public int completion_tokens;
-        public int total_tokens;
+        public int prompt_tokens {get;set;}
+        public int completion_tokens {get;set;}
+        public int total_tokens {get;set;}
     }
 
 }
