8a2332a5387ce0127eb25d33c77e7d03ecde2190
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2024-05-07 19:55:13 +0200

    fix: updated config file path

--- Assets/Config/ConfigLoader.cs
+++ Assets/Config/ConfigLoader.cs
@@ -1,13 +1,14 @@
+using System;
 using System.IO;
 using UnityEngine;
 
 namespace Config {
     public class ConfigLoader{
-        public static APISetting LoadConfig(string path ="./config.json" ) {
+        public static APISetting LoadConfig(string path = "Config/config" ) {
         //Get the JSON string from the file on disk.
-        string savedJson = File.ReadAllText(path);
+        var savedJson =  Resources.Load<TextAsset>(path);;
         //Convert the JSON string back to a ConfigData object.
-        return  JsonUtility.FromJson<APISetting>(savedJson);
+        return  JsonUtility.FromJson<APISetting>(savedJson.text);
         }
     }
 }
