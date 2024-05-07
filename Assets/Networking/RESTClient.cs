d319c979c0903d83890d08360edb6ec2f88bf427
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2024-05-07 19:55:13 +0200

    fix: updated config file path

--- Assets/Networking/RESTClient.cs
+++ Assets/Networking/RESTClient.cs
@@ -6,8 +6,6 @@ using System.Text;
 using Config;
 
 
-
-
 public class RESTClient {
     public static APISetting settings = ConfigLoader.LoadConfig();
     
@@ -19,6 +17,11 @@ public class RESTClient {
     }
 
     private static RESTClient _instance;
+    private static SystemRole baseSystemRole = new()
+    {
+        role = "system",
+        content = "You are a helpful assistant."
+    };
 
     public static RESTClient instance {
         get
@@ -29,37 +32,44 @@ public class RESTClient {
             return _instance;    
         }
     }
-
-    public async void sendGPT4PostRequest(string base64Image){
-        var endpoint = settings.base_url + settings.versionURL;
-        BaseImageDTO imageDTO = new BaseImageDTO
+    private BaseImageDTO createBase(string base64Image){
+        var imageDTO = new BaseImageDTO
         {
             max_tokens = 1000
         };
         imageDTO.messages.AddRange(
-            new GPTRoles[]{
-                new SystemRole(){
-                   role = "system",
-                   content ="You are a helpful assistant."
-                },
-                new UserRoleVision(){
-                    role = "user",
-                    content = new UserContent[]{
-                        new UserTextContent(){
-                            type = "text",
-                            text = "Describe this picture: ",
-                        },
-                        new UserVisionContent(){
-                            type ="image_url",
-                            image_url = new(){
-                                url ="data:image/jpeg;base64," + base64Image,
-                                detail ="low"
+                new GPTRoles[]{
+                    new SystemRole(){
+                    role = "system",
+                    content ="You are a helpful assistant."
+                    },
+                    new UserRoleVision(){
+                        role = "user",
+                        content = new UserContent[]{
+                            new UserTextContent(){
+                                type = "text",
+                                text = "Describe this picture: ",
+                            },
+                            new UserVisionContent(){
+                                type ="image_url",
+                                image_url = new(){
+                                    url ="data:image/jpeg;base64," + base64Image,
+                                    detail ="low"
+                                }
                             }
                         }
                     }
                 }
-            }
-        );
+            );
+        return imageDTO;
+    }
+
+    public async void sendGPT4PostRequest(string base64Image, BaseImageDTO imageDTO = null){
+        var endpoint = settings.base_url + settings.versionURL;
+        if(imageDTO == null){
+            imageDTO = createBase(base64Image);
+            
+        }
         Debug.Log(JsonConvert.SerializeObject(imageDTO));
         Debug.Log(client.DefaultRequestHeaders.ToString());
         var response = await client.PostAsync(endpoint, 
