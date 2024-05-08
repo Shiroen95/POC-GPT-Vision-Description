65c606fedd961d49368ebd6255cd8fa256324948
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2024-05-08 15:38:48 +0200

    feat: added DataScript

---
+++ Assets/Scripts/DataScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using Networking.DTO;
+using UnityEngine;
+
+namespace Scripts {
+    public class DataScript 
+    {   
+        public static Texture2D image;
+        public static BaseImageDTO request;
+        public static BaseResponseDTO response;
+    }
+}
