ffcdbf10d9130d2150ffb0d5bac62791e085badb
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2024-05-08 15:38:28 +0200

    feat: added nav script

---
+++ Assets/Scripts/NavigationScript.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public enum SceneType{
+    Input = 0,
+    Output = 2,
+    Camera = 1
+}
+public class NavigationScript : MonoBehaviour
+{
+
+    private int _currScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadSceneAsync("Scenes/SampleScene",LoadSceneMode.Additive);
+        _currScene = (int)SceneType.Camera;
+    }
+
+    [SerializeField]
+    public void changeScene(int sceneType){
+        SceneManager.UnloadSceneAsync(getScenePath(_currScene));
+        SceneManager.LoadSceneAsync(getScenePath(sceneType),LoadSceneMode.Additive);
+        _currScene = sceneType;
+    }
+
+    private string getScenePath(int sceneType){
+        string path = "Scenes/";
+        switch ((SceneType)sceneType)
+        {
+            case SceneType.Input:
+              return  path += "InputScene";
+            case SceneType.Output:
+                return path += "OutputScene";
+            default:
+            case SceneType.Camera:
+                 return path += "SampleScene";
+        }
+    }
+}
