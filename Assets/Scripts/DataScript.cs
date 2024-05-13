62416ce1bcb934cbe3c52d81e6f0f3debe140967
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2024-05-13 15:23:22 +0200

    feat: added init request

--- Assets/Scripts/DataScript.cs
+++ Assets/Scripts/DataScript.cs
@@ -7,7 +7,17 @@ namespace Scripts {
     public class DataScript 
     {   
         public static Texture2D image {get;set;}
-        public static BaseImageDTO request {get;set;}
+        public static BaseImageDTO request {get;set;} = new BaseImageDTO(){
+            max_tokens = 1000,
+            messages = new List<GPTRoles>{
+                new UserRoleVision(){
+                        content = new UserContent[]{
+                            new UserTextContent(){
+                                text = "Describe this picture: ",
+                            },
+                        }
+                    }},
+        };
         public static BaseResponseDTO response {get;set;}
     }
 }
