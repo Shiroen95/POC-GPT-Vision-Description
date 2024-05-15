c622832ac78e659483108fca93d1ae030b0b2db9
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2024-05-16 00:44:35 +0200

    feat: adds correct rotation

--- Assets/Scripts/CameraCaptureScript.cs
+++ Assets/Scripts/CameraCaptureScript.cs
@@ -16,7 +16,7 @@ public class CameraCaptureScript : MonoBehaviour
     [SerializeField]
     private Image _image;
     [SerializeField]
-    private GameObject sendingTxT;
+    private GameObject sendingText;
 
 
     public void takePicture(){
@@ -37,10 +37,12 @@ public class CameraCaptureScript : MonoBehaviour
     }
 
     public async void sendRequest (){
-        sendingTxT.SetActive(true);
+        sendingText.SetActive(true);
         await RESTClient.instance.sendGPT4PostRequest(Convert.ToBase64String(DataScript.image.EncodeToJPG()));
-        sendingTxT.SetActive(false);
+        sendingText.SetActive(false);
     }
+
+
 
     IEnumerator ConvertImageAsync(XRCpuImage image)
     {
@@ -52,15 +54,15 @@ public class CameraCaptureScript : MonoBehaviour
             inputRect = new RectInt(0, 0, image.width, image.height),
 
             // Optionally downsample by 2
-            outputDimensions = new Vector2Int(image.width/2, image.height/2),
+            outputDimensions = new Vector2Int(image.width, image.height),
 
             // Output an RGB color image format
             outputFormat = TextureFormat.RGBA32,
 
             // Flip across the Y axis
-            transformation = XRCpuImage.Transformation.MirrorX
-        });
+            transformation = XRCpuImage.Transformation.MirrorY
 
+        });
         // Wait for the conversion to complete
         while (!request.status.IsDone())
             yield return null;
@@ -89,10 +91,49 @@ public class CameraCaptureScript : MonoBehaviour
         // Copy the image data into the texture
         DataScript.image.LoadRawTextureData(rawData);
         DataScript.image.Apply();
+        RotateImage( DataScript.image,90f);
         _image.rectTransform.sizeDelta = request.conversionParams.outputDimensions;
-        _image.sprite = Sprite.Create(DataScript.image,new Rect(0, 0, image.width/2, image.height/2),Vector2.zero);
+        _image.sprite = Sprite.Create(DataScript.image,new Rect(0, 0, DataScript.image.width, DataScript.image.height),Vector2.zero);
         // Dispose the request including raw data
         request.Dispose();
+    }
+
+
+    public static void RotateImage(Texture2D tex, float angleDegrees)
+    {
+        int width = tex.width;
+        int height = tex.height;
+        float halfHeight = height * 0.5f;
+        float halfWidth = width * 0.5f;
+
+        var texels = tex.GetRawTextureData<Color32>();
+        var copy = System.Buffers.ArrayPool<Color32>.Shared.Rent(texels.Length);
+        Unity.Collections.NativeArray<Color32>.Copy(texels, copy, texels.Length);
+
+        float phi = Mathf.Deg2Rad * angleDegrees;
+        float cosPhi = Mathf.Cos(phi);
+        float sinPhi = Mathf.Sin(phi);
+
+        int address = 0;
+        for (int newY = 0; newY < height; newY++)
+        {
+            for (int newX = 0; newX < width; newX++)
+            {
+                float cX = newX - halfWidth;
+                float cY = newY - halfHeight;
+                int oldX = Mathf.RoundToInt(cosPhi * cX + sinPhi * cY + halfWidth);
+                int oldY = Mathf.RoundToInt(-sinPhi * cX + cosPhi * cY + halfHeight);
+                bool InsideImageBounds = (oldX > -1) & (oldX < width)
+                                       & (oldY > -1) & (oldY < height);
+
+                texels[address++] = InsideImageBounds ? copy[oldY * width + oldX] : default;
+            }
+        }
+
+        // No need to reinitialize or SetPixels - data is already in-place.
+        tex.Apply(true);
+
+        System.Buffers.ArrayPool<Color32>.Shared.Return(copy);
     }
 
 }
