95d97a5fbe8a797e207630e8bb3d03620ab95733
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2024-05-16 15:17:12 +0200

    feat: created basic singleton

--- Assets/Scripts/IO/ExportScript.cs
+++ Assets/Scripts/IO/ExportScript.cs
@@ -1,19 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ExportScript : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
-    void Start()
-    {
+    private ExportScript(){
         
+    }
+    private static ExportScript _instance;
+    public static ExportScript instance{get
+        {
+            if(_instance==null){
+                _instance = new ExportScript();
+            }
+            return _instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
