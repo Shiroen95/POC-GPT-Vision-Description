c79bc9ffb212bdc31f5955ac70d59698719e4b2e
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2024-06-18 21:53:06 +0200

    feat: added basic import function.

--- Assets/Scripts/IO/ImportScript.cs
+++ Assets/Scripts/IO/ImportScript.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Networking.DTO;
+using Newtonsoft.Json;
+using Scripts;
 using UnityEngine;
 
 public class ImportScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private string jsonFileType;
+
     void Start()
     {
-        
+        jsonFileType = NativeFilePicker.ConvertExtensionToFileType( "json" ); // Returns "application/pdf" on Android and "com.adobe.pdf" on iOS
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public async void importSetting(){
+        var path = pickFile();
+        if (path != ""){
+            var jsonString = await LoadJson(path);
+            DataScript.request = JsonConvert.DeserializeObject<BaseImageDTO>(jsonString);
+        }
     }
+    
+    private string pickFile(){
+        var filePath = "";
+        NativeFilePicker.Permission permission = NativeFilePicker.PickFile( ( path ) =>
+			{
+				if( path == null )
+					Debug.Log( "Operation cancelled" );
+				else
+					filePath = path;
+			}, new string[] { jsonFileType } );
+
+			return filePath;
+    }
+
+    public async Task<string> LoadJson(string path)
+    {
+        using (StreamReader r = new StreamReader(path))
+        {
+            return await r.ReadToEndAsync();
+        }
+    }
+
+
 }
