3f4c8e03dfc2619569983ad05ee74d44c13bb3b6
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2024-07-02 23:20:20 +0200

    feat: adds on click and add method

--- Assets/Scripts/Demo/DTO/CleaningTask.cs
+++ Assets/Scripts/Demo/DTO/CleaningTask.cs
@@ -1,5 +1,11 @@
 public class CleaningTask{
+    
     public CleaningTask(){}
+
+    public CleaningTask(string Name, string Description){
+        this.Name = Name;
+        this.Description = Description;
+    }
 
     public string Name{get;set;}
     public string Description{get;set;}
