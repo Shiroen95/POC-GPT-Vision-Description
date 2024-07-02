cbc7af8b48c7e9ab6daa2f13b7041ffad4757b62
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2024-07-03 00:23:32 +0200

    feat: added data script logic

--- Assets/Scripts/Demo/DemoDataScript.cs
+++ Assets/Scripts/Demo/DemoDataScript.cs
@@ -1,29 +1,64 @@
 using System.Collections.Generic;
 
-public class DemoDataScript{
-    private int index; 
-    private Dictionary<int,CleaningTask> taskList;
+namespace Demo{
 
-    private DemoDataScript(){
-        createNewDataList();
+    public enum modifyMode{
+        edit,
+        create, 
+        none
     }
 
-    private void createNewDataList(){
-        taskList = new Dictionary<int,CleaningTask>();
-        index = 0;
-    }
-    private static DemoDataScript _instance = new DemoDataScript();
+    public class DemoDataScript{
+        public modifyMode currModifyMode = modifyMode.none;
+        private int index;
+        private Dictionary<int,CleaningTask> taskList;
+        private int _currCleaningTaskIndex = -1; 
+        private static DemoDataScript _instance = new DemoDataScript();
+        public static DemoDataScript Instance {
+            get => _instance;
+        }
+        public CleaningTask currCleaningTask {
+            get => _currCleaningTaskIndex==-1 ? null : GetCleaningTask(_currCleaningTaskIndex);
+        }
 
-    public static DemoDataScript Instance {
-        get => _instance;
-    }
+        private DemoDataScript(){
+            var loadedData = checkForInternalData();
+            if(loadedData != null)
+            {
+                taskList = loadedData;
+                index = loadedData.Count;
+            }
+            else
+                createNewDataList();
+        }
 
-    public (int,CleaningTask) addCleaningTask(CleaningTask cleaningTask){
-        taskList.Add(index,cleaningTask);
-        var returnValue = (index,cleaningTask);
-        index++;
-        return returnValue;
-    }
+        private void createNewDataList(){
+            taskList = new Dictionary<int,CleaningTask>();
+            index = 0;
+        }
 
-    public CleaningTask GetCleaningTask(int index) => taskList.GetValueOrDefault(index);
+        private Dictionary<int,CleaningTask> checkForInternalData(){
+            return null;
+        }
+        public void setCurrentCleaningTask(int index){
+            if(taskList.ContainsKey(index))
+                _currCleaningTaskIndex = index;
+        }
+        public (int,CleaningTask) addCleaningTask(CleaningTask cleaningTask){
+            taskList.Add(index, cleaningTask);
+            var returnValue = (index, cleaningTask);
+            index++;
+            return returnValue;
+        }
+
+        public (int,CleaningTask) modifyCleaningTask(CleaningTask cleaningTask, int index){
+            if(taskList.ContainsKey(index))
+                taskList[index] = cleaningTask;
+
+            return(index, cleaningTask);
+        }
+
+        public CleaningTask GetCleaningTask(int index) => taskList.GetValueOrDefault(index);
+        
+    }
 }
