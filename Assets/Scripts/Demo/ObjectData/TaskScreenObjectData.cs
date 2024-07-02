ca39e93b798cfd7cffd214447235106bdd84629c
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2024-07-03 00:44:31 +0200

    feat: added simple manual task add and edit

--- Assets/Scripts/Demo/ObjectData/TaskScreenObjectData.cs
+++ Assets/Scripts/Demo/ObjectData/TaskScreenObjectData.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -7,19 +5,8 @@ namespace Demo.DataObject{
     public class TaskScreenObjectData : MonoBehaviour
     {
         [SerializeField]
-        TMP_InputField headlineIf;
+        public TMP_InputField headlineIf;
         [SerializeField]
-        TMP_InputField descriptionIf;
-        // Start is called before the first frame update
-        void Start()
-        {
-            
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            
-        }
+        public TMP_InputField descriptionIf;
     }
 }
