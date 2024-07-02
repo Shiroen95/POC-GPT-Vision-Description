bc4463fa70ef441b558bec1a019ec9cd482a1d93
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2024-07-03 00:32:39 +0200

    feat: adds values for Task screen objects

--- Assets/Scripts/Demo/ObjectData/TaskScreenObjectData.cs
+++ Assets/Scripts/Demo/ObjectData/TaskScreenObjectData.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 namespace Demo.DataObject{
     public class TaskScreenObjectData : MonoBehaviour
     {
+        [SerializeField]
+        TMP_InputField headlineIf;
+        [SerializeField]
+        TMP_InputField descriptionIf;
         // Start is called before the first frame update
         void Start()
         {
