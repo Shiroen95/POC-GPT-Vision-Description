4d08a75094f93a6824b77eb65fe70be8d6a992e0
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2024-07-04 01:45:12 +0200

    feat: added generation tag method

--- Assets/Scripts/Demo/ObjectData/UserInputObjectData.cs
+++ Assets/Scripts/Demo/ObjectData/UserInputObjectData.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,4 +10,6 @@ public class UserInputObjectData : MonoBehaviour
     public Image image;
     [SerializeField]
     public GameObject tagTemplate;
+    [SerializeField]
+    public GameObject tagList;
 }
