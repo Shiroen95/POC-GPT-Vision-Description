4e796281a3bf31b622ef3913c72cc2b6cd84bddd
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2024-07-04 03:11:16 +0200

    chore: changes color for select and deselect

--- Assets/Scripts/Demo/TagController.cs
+++ Assets/Scripts/Demo/TagController.cs
@@ -15,6 +15,6 @@ public class TagController : MonoBehaviour
     // Start is called before the first frame update
     public void onSelect(){
         selected = !selected;
-        tagImage.color = selected? Color.blue : Color.green;
+        tagImage.color = selected? new Color32(0x2A,0x00,0x91,0xFF) : new Color32(0x49,0x00,0xFF,0xFF);
     }
 }
