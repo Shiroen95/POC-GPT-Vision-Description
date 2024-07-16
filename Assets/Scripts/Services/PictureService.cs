a96d39ccf56519b72ae62ae8606e11b53bf52225
Hermann Fröhlich <hermann.froehlich@uni-ulm.de>
2024-07-16 17:53:21 +0200

    feat: added windows file picker

--- Assets/Scripts/Services/PictureService.cs
+++ Assets/Scripts/Services/PictureService.cs
@@ -1,34 +1,85 @@
+
 using System;
+using SFB;
 using UnityEngine;
 namespace Services{
     public class PictureService
     {
-        public static void PickImage(int maxImgSize, Action<Texture2D> callback)
-        {
-            Texture2D texture = null;
-            NativeGallery.Permission permission = NativeGallery.GetImageFromGallery( ( path ) =>
+    public static void PickImage(int maxImgSize, Action<Texture2D> callback)
+    {
+        Texture2D texture = null;
+#if UNITY_STANDALONE_WIN || UNITY_EDITOR
+        // Open file async
+        var extensions = new [] { new ExtensionFilter("Image Files", "png", "jpg", "jpeg" )};
+        StandaloneFileBrowser.OpenFilePanelAsync("Open File", "", extensions, false, (string[] paths) => {
+            if(paths.Length == 0) 
+                return; 
+            var path = paths[0];
+            Debug.Log( "Image path: " + path );
+            if( path != null )
             {
-                Debug.Log( "Image path: " + path );
-                if( path != null )
+                // Create Texture from selected image
+                texture = NativeGallery.LoadImageAtPath(path, maxSize:maxImgSize ,false);
+                if( texture == null )
                 {
-                    // Create Texture from selected image
-                    texture = NativeGallery.LoadImageAtPath(path, maxSize:maxImgSize ,false);
-                    if( texture == null )
-                    {
-                        Debug.Log( "Couldn't load texture from " + path );
-                        return;
-                    }
-                    Debug.Log("height: " + texture.height);
-                    Debug.Log("width: " + texture.width);
-                    callback(texture);
+                    Debug.Log( "Couldn't load texture from " + path );
+                    return;
                 }
-            } );
-        }
-
-
+                Debug.Log("height: " + texture.height);
+                Debug.Log("width: " + texture.width);
+                callback(texture);
+            }
+        });
+        return;
+#else
+        
+        NativeGallery.Permission permission = NativeGallery.GetImageFromGallery( ( path ) =>
+        {
+            Debug.Log( "Image path: " + path );
+            if( path != null )
+            {
+                // Create Texture from selected image
+                texture = NativeGallery.LoadImageAtPath(path, maxSize:maxImgSize ,false);
+                if( texture == null )
+                {
+                    Debug.Log( "Couldn't load texture from " + path );
+                    return;
+                }
+                Debug.Log("height: " + texture.height);
+                Debug.Log("width: " + texture.width);
+                callback(texture);
+            }
+        } );
+#endif
+    }
 
     public static void TakePicture( int maxSize,Action<Texture2D> callback )
     {
+        Texture2D texture = null;
+#if UNITY_STANDALONE_WIN || UNITY_EDITOR
+        // Open file async
+        var extensions = new [] { new ExtensionFilter("Image Files", "png", "jpg", "jpeg" )};
+        StandaloneFileBrowser.OpenFilePanelAsync("Open File", "", extensions, false, (string[] paths) => {
+            if(paths.Length == 0) 
+                return; 
+            var path = paths[0];
+            Debug.Log( "Image path: " + path );
+            if( path != null )
+            {
+                // Create Texture from selected image
+                texture = NativeGallery.LoadImageAtPath(path, maxSize:maxSize ,false);
+                if( texture == null )
+                {
+                    Debug.Log( "Couldn't load texture from " + path );
+                    return;
+                }
+                Debug.Log("height: " + texture.height);
+                Debug.Log("width: " + texture.width);
+                callback(texture);
+            }
+        });
+        return;
+#else
         Texture2D texture = null;
         NativeCamera.Permission permission = NativeCamera.TakePicture( ( path ) =>
         {
@@ -45,15 +96,8 @@ namespace Services{
                 callback(texture);
             }
         }, maxSize );
+    #endif
     }
-
-
-
-
-
-
-
-
 
         
     }
